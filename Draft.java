c3dfbf181c24ebc8f29d14a24d6e2294a9e634e7
duncanzeller <163003449+duncanzeller@users.noreply.github.com>
2024-11-17 23:35:26 -0500

    Update Draft.java

--- Draft.java
+++ Draft.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Draft {
 	
-	private HashSet<Player> draftedPlayers;
-	private HashMap<Integer, Team> draftOrder;
+	private static HashSet<Player> draftedPlayers;
+	private static HashMap<Integer, Team> draftOrder;
 	private Team[] teams;
 	Random rand = new Random();
 	
@@ -13,66 +13,67 @@ public class Draft {
 		teams = new Team[30];
 		draftOrder = new HashMap<Integer, Team>();
 		
-		Team az = new Team("Dimondbacks");
-		teams[0] = az;
-		Team oak = new Team("Athletics");
-		teams[1] = oak;
-		Team atl = new Team("Braves");
-		teams[2] = atl;
-		Team bal = new Team("Orioles");
-		teams[3] = bal;
-		Team bos = new Team("Red Sox");
-		teams[4] = bos;
-		Team cubs = new Team("Cubs");
-		teams[5] = cubs;
-		Team whiteSox = new Team("White Sox");
-		teams[6] = whiteSox;
-		Team cinci = new Team("Reds");
-		teams[7] = cinci;
-		Team clev = new Team("Guardians");
-		teams[8] = clev;
-		Team col = new Team("Rockies");
-		teams[9] = col;
-		Team det = new Team("Tigers");
-		teams[10] = det;
-		Team hou = new Team("Astros");
-		teams[11] = hou;
-		Team kc = new Team("Royals");
-		teams[12] = kc;
-		Team laA = new Team("Angels");
-		teams[13] = laA;
-		Team laD = new Team("Dodgers");
-		teams[14] = laD;
-		Team mia = new Team("Marlins");
-		teams[15] = mia;
-		Team mil = new Team("Brewers");
-		teams[16] = mil;
-		Team min = new Team("Twins");
-		teams[17] = min;
-		Team nyM = new Team("Mets");
-		teams[18] = nyM;
-		Team nyY = new Team("Yankees");
-		teams[19] = nyY;
-		Team phil = new Team("Phillies");
-		teams[20] = phil;
-		Team pit = new Team("Pirates");
-		teams[21] = pit;
-		Team sd = new Team("Padres");
-		teams[22] = sd;
-		Team sf = new Team("Giants");
-		teams[23] = sf;
-		Team sea = new Team("Mariners");
-		teams[24] = sea;
-		Team stl = new Team("Cardinals");
-		teams[25] = stl;
-		Team tb = new Team("Rays");
-		teams[26] = tb;
-		Team tx = new Team("Rangers");
-		teams[27] = tx;
-		Team tor = new Team("Blue Jays");
-		teams[28] = tor;
-		Team wash = new Team("Nationals");	
-		teams[29] = wash;
+		Team Diamondbacks = new Team("Diamondbacks");
+		teams[0] = Diamondbacks;
+		Team Athletics = new Team("Athletics");
+		teams[1] = Athletics;
+		Team Braves = new Team("Braves");
+		teams[2] = Braves;
+		Team Orioles = new Team("Orioles");
+		teams[3] = Orioles;
+		Team RedSox = new Team("Red Sox");
+		teams[4] = RedSox;
+		Team Cubs = new Team("Cubs");
+		teams[5] = Cubs;
+		Team WhiteSox = new Team("White Sox");
+		teams[6] = WhiteSox;
+		Team Reds = new Team("Reds");
+		teams[7] = Reds;
+		Team Guardians = new Team("Guardians");
+		teams[8] = Guardians;
+		Team Rockies = new Team("Rockies");
+		teams[9] = Rockies;
+		Team Tigers = new Team("Tigers");
+		teams[10] = Tigers;
+		Team Astros = new Team("Astros");
+		teams[11] = Astros;
+		Team Royals = new Team("Royals");
+		teams[12] = Royals;
+		Team Angels = new Team("Angels");
+		teams[13] = Angels;
+		Team Dodgers = new Team("Dodgers");
+		teams[14] = Dodgers;
+		Team Marlins = new Team("Marlins");
+		teams[15] = Marlins;
+		Team Brewers = new Team("Brewers");
+		teams[16] = Brewers;
+		Team Twins = new Team("Twins");
+		teams[17] = Twins;
+		Team Mets = new Team("Mets");
+		teams[18] = Mets;
+
+		Team Yankees = new Team("Yankees");
+		teams[19] = Yankees;
+		Team Phillies = new Team("Phillies");
+		teams[20] = Phillies;
+		Team Pirates = new Team("Pirates");
+		teams[21] = Pirates;
+		Team Padres = new Team("Padres");
+		teams[22] = Padres;
+		Team Giants = new Team("Giants");
+		teams[23] = Giants;
+		Team Mariners = new Team("Mariners");
+		teams[24] = Mariners;
+		Team Cardinals = new Team("Cardinals");
+		teams[25] = Cardinals;
+		Team Rays = new Team("Rays");
+		teams[26] = Rays;
+		Team Rangers = new Team("Rangers");
+		teams[27] = Rangers;
+		Team BlueJays = new Team("Blue Jays");
+		teams[28] = BlueJays;
+		Team Nationals = new Team("Nationals");
+		teams[29] = Nationals;
 		
 		while(draftOrder.size() < 30) {
 			for(int i = 0; i < 30; i++) {
@@ -80,9 +81,8 @@ public class Draft {
 			}
 		}
 		
-		
 		System.out.println("Welcome to the 2024 MLB Draft");
-		System.out.println("The " + draftOrder.get(0).getTeamName() + " are on the clock:");
+
 			
 	}
 	
@@ -116,7 +116,7 @@ public class Draft {
 	public void printPitchers() {
 		for(Player p : draftedPlayers) {
 			if (p.getPosition().equalsIgnoreCase("P")) {
-				System.out.print(p.getName() + " ");
+				System.out.print(p.getName() + ", ");
 			}
 		}
 	}
@@ -124,7 +124,7 @@ public class Draft {
 	public void printHitters() {
 		for(Player p : draftedPlayers) {
 			if(!p.getPosition().equalsIgnoreCase("P")) {
-				System.out.print(p.getName() + " ");
+				System.out.print(p.getName() + ", ");
 			}
 		}
 	}
@@ -134,54 +134,72 @@ public class Draft {
 		
 		Scanner input = new Scanner(System.in);
 		Random random = new Random();
+		String name = "";
+		int num = 999;
+		String school = "";
+		String pos = "";
 		Draft d = new Draft();
 		d.startDraft();
 		//boolean draftDone = false;
-		
 		for(int i = 0; i < 30; i++) {
-			System.out.println("Who would you like to draft:");
+			int pickIn = 0;
+			while(pickIn == 0) {
+			System.out.println("The " + draftOrder.get(i).getTeamName() + " are on the clock:");
+
+			System.out.println("Who would you like to draft?");
 			System.out.println("Name:");
-			String name = input.nextLine();
-			System.out.println("Position (enter abbriviation ex. C, P, etc.): ");
-			String pos = input.nextLine();
+			name = input.nextLine();
+			
+			
+			System.out.println("Position (enter abbreviation ex. C, P, etc.): ");
+			pos = input.nextLine();
 			System.out.println("Number:");
-			int num = input.nextInt();
+			num = input.nextInt();
 			input.nextLine();
 			System.out.println("School:");
-			String school = input.nextLine();
+			school = input.nextLine();
 			
+			//check to see if player was already drafted
+			if(i != 0) {
+				for(Player p: draftedPlayers) {
+				
+					Player check = new Player(name,num,school,pos);
+					if(p.equals(check)) {
+						System.out.println("The player has already been drafted. Please pick another player");
+						break;
+					}
+					else {
+						pickIn = 1;
+					}
+					
+				}
+			}
+			else {
+				pickIn = 1;
+			}
+			}
 			if(pos.equalsIgnoreCase("P")){
 				System.out.println("Please enter pitchers era:");
 				double era = input.nextDouble();
 				input.nextLine();
 				Pitcher p = new Pitcher(name, num, school, pos, era);
-				if(d.getDraftedPlayers().contains(p)) {
-					System.out.println("This player has already been drafted");
-				} else {
+				
 					d.draftPlayer(p);
-					d.draftOrder.get(i).draftPlayer(p);
+					draftOrder.get(i).draftPlayer(p);
 					p.print();
 					System.out.println();
-					if((i+1) != 30) {
-						System.out.println(d.getNextPick(i).getTeamName() + " are on the clock:");
-					}
-					
-				}
+				
 			} else {
 				System.out.println("Please enter hitters batting average:");
 				double ba = input.nextDouble();
 				input.nextLine();
 				Hitter h = new Hitter(name, num, school, pos, ba);
-				if (d.getDraftedPlayers().contains(h)) {
-					System.out.println("This player has already been drafted");
-				} else {
-					d.draftPlayer(h);
-					h.print();
-					System.out.println();
-					if((i+1) != 30) {
-						System.out.println(d.getNextPick(i).getTeamName() + " are on the clock:");
-					}
-				}
+				
+				d.draftPlayer(h);
+				draftOrder.get(i).draftPlayer(h);
+				h.print();
+				System.out.println();
+				
 			}
 			
 		}
@@ -190,9 +208,9 @@ public class Draft {
 		System.out.println();
 		System.out.println("The draft has been completed:");
 		while(!isDone) {
-			System.out.println("The following options are avalible:");
+			System.out.println("The following options are available:");
 			System.out.println("1. Print a teams drafted players:");
-			System.out.println("2. Print out all hitter/pitcher from draft class:");
+			System.out.println("2. Print out all hitters or all pitchers from draft class:");
 			System.out.println("3. Print out the entire 2024 draft class:");
 			System.out.println("4. Exit the draft");
 			int option = input.nextInt();
@@ -220,7 +238,7 @@ public class Draft {
 				break;
 				
 			case 2:
-				System.out.println("Would you like to print out hitters or pitchrs:");
+				System.out.println("Would you like to print out hitters or pitchers:");
 				String printPos = input.nextLine();
 				System.out.println();
 				if(printPos.equalsIgnoreCase("Pitcher")) {
@@ -237,7 +255,7 @@ public class Draft {
 			case 3:
 
 				for(Player p: d.getDraftedPlayers()) {
-					System.out.print(p.getName() + " ");
+					System.out.println(p.getName());
 				}
 				System.out.println();
 				
